d583f2423d56001926799111fbe17f17dacb4084
F4TAM <91732905+F4TAM@users.noreply.github.com>
2022-08-31 14:08:43 +0200

    Added namespace Library
    template print function

--- readAndWrite/src/Main.cpp
+++ readAndWrite/src/Main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
+
+
+
+namespace Library 
+{
+	template <typename T> 
+	void print(T t)
+	{
+		std::cout << t << "\n";
+	}
+}
+
+
+
 int main()
 {
-
+	Library::print("Hej");
 }
